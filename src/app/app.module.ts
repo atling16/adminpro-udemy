d74c659362bb41a30fd0abc308d6108b413bb4b8
Aaron Jeronimo Torres Acosta <e-ajtorresa@neoris.cxnetworks.net>
2019-05-16 15:45:39 -0500

    modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { LogingComponent } from './loging/loging.component';
+import { RegisterComponent } from './loging/register.component';
 
 // Rutas
 import { APP_ROUTES } from './app.routes';
 
-
-import { LogingComponent } from './loging/loging.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefundComponent } from './shared/nopagefund/nopagefund.component';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './loging/register.component';
-
-
-
+ // Modulos
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
+
     AppComponent,
     LogingComponent,
-    NopagefundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    RegisterComponent,
+    RegisterComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
